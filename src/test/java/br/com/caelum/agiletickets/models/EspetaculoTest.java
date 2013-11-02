c2c1e52644b1219b2bc63fd71f7f5e90167ab3b5
agil3763 <agil3763@caelum-Satellite-L655>
2013-11-02 12:43:36 -0200

    mensagem

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertTrue;
 import java.util.List;
 
 import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
 import org.joda.time.LocalTime;
 import org.junit.Test;
 
@@ -102,49 +101,5 @@ public class EspetaculoTest {
 		assertEquals( "28/10/13", sessao.getDia() );
 		assertEquals( "12:00", sessao.getHora() );
 	}
-	
-	@Test
-	public void ehPossivelDuasSessoes() {
-		LocalDate inicio = new LocalDate(2013,10,1); 
-		LocalDate fim = new LocalDate(2013,10,3); 
-		LocalTime horario =new LocalTime(12,00,00);
-		Periodicidade periodicidade = Periodicidade.DIARIA;
-		
-		Espetaculo espetaculo = new Espetaculo();
-		List<Sessao> sessoes = espetaculo.criaSessoes(inicio, fim, horario, periodicidade);
-		
-		assertEquals(2 ,sessoes.size());
-		Sessao sessao = sessoes.get(0);
-		
-		assertEquals( "01/10/13", sessao.getDia() );
-		assertEquals( "12:00", sessao.getHora() );
-		
-		Sessao sessao1 = sessoes.get(1);
-		assertEquals( "03/10/13", sessao1.getDia() );
-		assertEquals( "12:00", sessao1.getHora() );
-	}
-	
-	@Test
-	public void ehPossivelSessoesSemanais() {
-		LocalDate inicio = new LocalDate(2013,10,1); 
-		LocalDate fim = new LocalDate(2013,10,8); 
-		LocalTime horario =new LocalTime(12,00,00);
-		Periodicidade periodicidade = Periodicidade.SEMANAL;
-		
-		Espetaculo espetaculo = new Espetaculo();
-		List<Sessao> sessoes = espetaculo.criaSessoes(inicio, fim, horario, periodicidade);
-		
-		assertEquals(2 ,sessoes.size());
-		Sessao sessao = sessoes.get(0);
-		
-		assertEquals( "01/10/13", sessao.getDia() );
-		assertEquals( "12:00", sessao.getHora() );
-		
-		Sessao sessao1 = sessoes.get(1);
-		assertEquals( "08/10/13", sessao1.getDia() );
-		assertEquals( "12:00", sessao1.getHora() );
-	}
-	
-	
 
 }
